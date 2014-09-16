e8785372de2bd5196b1fe2798292e50a45cf822d
Subramanian Olagappan <subramaniano@marketshare.com>
2014-09-16 09:43:56 +0530

    Revert "My wild guess to get the build working"
    This reverts commit fb5ed95345de93745e38dd214209ec69c6b2fd67.

--- ThreeSixtyOneView/Gruntfile.js
+++ ThreeSixtyOneView/Gruntfile.js
@@ -188,7 +188,7 @@ module.exports = function(grunt) {
                 files: {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
-                        '<%= yeoman.dist %>/styles/{,*/}*.css',
+                        // '<%= yeoman.dist %>/styles/{,*/}*.css',
                         // '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
                         '<%= yeoman.dist %>/styles/fonts/*'
                     ]
