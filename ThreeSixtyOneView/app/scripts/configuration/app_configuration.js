9ab50566a456e04c8f141bcd0bbb7f985a0755b8
Mark Gable <mgable@marketshare.com>
2014-09-29 17:34:56 -0700

    MAR-1936 and 1937

--- ThreeSixtyOneView/app/scripts/configuration/app_configuration.js
+++ ThreeSixtyOneView/app/scripts/configuration/app_configuration.js
@@ -12,10 +12,12 @@ angular.module('ThreeSixtyOneView.config')
             "models": {
                 "ProjectsModel": { // want: get
                     "responseTranslator": {"isMaster": "isMaster", "id" : "uuid", "title": "name", "description": "description", "createdBy":{"selector":"['auditInfo']['createdBy']['name']"}, "createdOn":{"selector":"['auditInfo']['createdOn']"}, "modifiedBy":{"selector":"['auditInfo']['lastUpdatedBy']['name']"}, "modifiedOn":{"selector":"['auditInfo']['lastUpdatedOn']"}},
-                    "requestTranslator": {"uuid" : "id", "name": "title", "description": "description", "isMaster": "isMaster"}
+                    "requestTranslator": {"uuid" : "id", "name": "title", "description": "description", "isMaster": "isMaster"},
+                    "newProject": {"title": "","description" : "", "isMaster": false}
                 },
                 "ScenarioModel": {
-                    "responseTranslator": {"title": "name", "id": "id", "description": "description", "type":{"selector":"['prediction']['type']"}, "createdBy":{"selector":"['auditInfo']['createdBy']['name']"}, "createdOn":{"selector":"['auditInfo']['createdOn']"}, "modifiedBy":{"selector":"['auditInfo']['lastUpdatedBy']['name']"}, "modifiedOn":{"selector":"['auditInfo']['lastUpdatedOn']"}}
+                    "responseTranslator": {"title": "name", "id": "id", "description": "description", "type":{"selector":"['prediction']['type']"}, "createdBy":{"selector":"['auditInfo']['createdBy']['name']"}, "createdOn":{"selector":"['auditInfo']['createdOn']"}, "modifiedBy":{"selector":"['auditInfo']['lastUpdatedBy']['name']"}, "modifiedOn":{"selector":"['auditInfo']['lastUpdatedOn']"}},
+                    "newScenario": {"name" : "", "description": "","referenceScenario": {"id": 1, "name": "Master Scenario"}, "prediction" : {"type" : "Simulation"}}
                 }
             }
         },
@@ -82,11 +84,6 @@ angular.module('ThreeSixtyOneView.config')
                 "status": false,
                 "where": 'gotoDashboard',
                 "displayActionsCreate": "scope.DiaglogService.create('project')",
-                "newProject": {
-                    "title": "",
-                    "description" : "",
-                    "isMaster": false
-                },
                 "filterMenu": {
                     "firstSelected": 0,
                     "icon": "suitcase",
