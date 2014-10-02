0f5c0f9788db20df320f1d97d3156c665577ed5d
Mark Gable <mgable@marketshare.com>
2014-10-02 13:20:22 -0700

    refactored models

--- ThreeSixtyOneView/app/scripts/services/favorites_model.js
+++ ThreeSixtyOneView/app/scripts/services/favorites_model.js
@@ -2,40 +2,29 @@
 /* jshint unused:false */
 'use strict';
 
-    angular.module('ThreeSixtyOneView.services').service('FavoritesModel', ["$timeout", "$rootScope", "$location", "Resource", "CONFIG", "SERVER", function($timeout, $rootScope, $location, Resource, CONFIG, SERVER){
-        var resource = new Resource(SERVER[$location.host()]  + CONFIG.application.api.favorites),
-        // translator = CONFIG.application.models.ProjectsModel.translator, 
-        config = {},
-        unwrap = function(futureData) {
-            var self = this;
-            this.$futureData = futureData;
-            this.$futureData.then(function(data) {
-                $timeout(function() {
-                    _.extend(self, data);
-                });
-            });
-        };
+angular.module('ThreeSixtyOneView.services').service('FavoritesModel', ["$location", "Resource", "CONFIG", "SERVER", "ModelModel", function($location, Resource, CONFIG, SERVER, ModelModel){
+    var resource = new Resource(SERVER[$location.host()]  + CONFIG.application.api.favorites), config = {};
 
-        this.find = function(id) {
-            unwrap.call(this, resource.get(id, config));
-        };
+    this.find = function(id) {
+        ModelModel.unwrap.call(this, resource.get(id, config));
+    };
 
-        this.get = function() {
-            return this.$futureData;
-        };
+    this.get = function() {
+        return this.$futureData;
+    };
 
-        this.setAsFavorite = function(id) {
-            resource.post({'uuid': id}, config).then(function(response){
-                // TODO: see error responses
-                //console.info(response);
-            });
-        };
+    this.setAsFavorite = function(id) {
+        resource.post({'uuid': id}, config).then(function(response){
+            // TODO: see error responses
+            //console.info(response);
+        });
+    };
 
-        this.unFavorite = function(id){
-            var params = {params:{"uuid": id}};
-            resource.delete(params, config).then(function(response){
-                console.info(response);
-            });
-        };
+    this.unFavorite = function(id){
+        var params = {params:{"uuid": id}};
+        resource.delete(params, config).then(function(response){
+            console.info(response);
+        });
+    };
 
-    }]);
+}]);
