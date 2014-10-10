f85dc12303bfc5981d66f9f67d06e6c25dfcc3a0
Mark Gable <mgable@marketshare.com>
2014-10-10 11:25:35 -0700

    fixed build system

--- ThreeSixtyOneView/Gruntfile.js
+++ ThreeSixtyOneView/Gruntfile.js
@@ -288,7 +288,19 @@ module.exports = function(grunt) {
         // ngmin tries to make the code safe for minification automatically by
         // using the Angular long form for dependency injection. It doesn't work on
         // things like resolve or inject so those have to be done manually.
-        ngmin: {
+        // ngmin: {
+        //     dist: {
+        //         files: [{
+        //             expand: true,
+        //             cwd: '.tmp/concat/scripts',
+        //             src: '*.js',
+        //             dest: '.tmp/concat/scripts'
+        //         }]
+        //     }
+        // },
+
+        ngAnnotate: {
+            options: {},
             dist: {
                 files: [{
                     expand: true,
@@ -444,7 +456,8 @@ module.exports = function(grunt) {
         'concurrent:dist',
         'autoprefixer',
         'concat',
-        'ngmin',
+        // 'ngmin',
+        'ngAnnotate',
         'copy:dist',
         'cdnify',
         'cssmin',
