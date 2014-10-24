36fb08a071ad6e026731a772c56808e24e9d2fc9
Hector Alvarez <hector.alvarez@naviscent.com>
2014-10-24 10:01:32 -0700

    filter fixes

--- ThreeSixtyOneView/app/scripts/directives/display_actions.js
+++ ThreeSixtyOneView/app/scripts/directives/display_actions.js
@@ -3,37 +3,56 @@
 'use strict';
 
 angular.module('ThreeSixtyOneView.directives')
-    .directive("displayActions", ["$rootScope", "CONFIG", "$state", "EVENTS", "SortAndFilterService", function($rootScope, CONFIG, $state, EVENTS, SortAndFilterService) {
+    .directive("displayActions", ["$document","$timeout","$rootScope", "CONFIG", "$state", "EVENTS", "SortAndFilterService", function($document,$timeout,$rootScope, CONFIG, $state, EVENTS, SortAndFilterService) {
         return {
             restrict: "AE",
             replace: true,
             templateUrl: "views/directives/display_actions.tpl.html",
-            link: function(scope, element, attrs) {
+            controller: function($scope, $element, $attrs) {
                 // Bootstrap
-                scope.view = {};
-                scope.view.create = false;
-                scope.view.filter = false;
-                scope.view.search = false;
-                scope.CONFIG = CONFIG.view[$state.current.name];
-                scope.SortAndFilterService = SortAndFilterService;
+                $scope.view = {};
+                $scope.view.create = false;
+                $scope.view.filter = false;
+                $scope.view.search = false;
+                $scope.CONFIG = CONFIG.view[$state.current.name];
+                $scope.SortAndFilterService = SortAndFilterService;
+                var filterDropdown = $($element).find('.filterDropdown');
 
-                var show = scope.$eval(attrs.show);
+                var show = $scope.$eval($attrs.show);
+
+                function close () {
+                    filterDropdown.addClass('hide');
+                    $document.off('click', close);
+                }
 
                 function toggleActions(which) {
                     _.each(show, function(e, i, a) {
-                        scope.view[e] = true;
+                        $scope.view[e] = true;
                     });
                 }
 
                 toggleActions(show);
                 
-                scope.create = function(action, data) {
+                $scope.create = function(action, data) {
                     //action is and EVENTS.type
                     /* jshint ignore:start */
                     //eval(action);
                     /* jshint ignore:end */
                     $rootScope.$broadcast(EVENTS[action], data);
                 };
+
+                $scope.toggle = function() {
+                   if (filterDropdown.hasClass('hide')) {
+                        filterDropdown.removeClass('hide');
+                        $timeout(function() {
+                            $document.on('click', close);
+                        });
+                    } else {
+                        filterDropdown.addClass('hide');
+                        $document.off('click', close);
+                    }
+                };
+
             }
 
         };
--- ThreeSixtyOneView/app/views/templates/templates.js
+++ ThreeSixtyOneView/app/views/templates/templates.js
@@ -2,166 +2,98 @@ angular.module('ThreeSixtyOneView').run(['$templateCache', function($templateCac
   'use strict';
 
   $templateCache.put('views/directives/display_actions.tpl.html',
-    "<div class=\"display-actions\">\r" +
+    "<div class=\"display-actions\">\n" +
+    "\t<h4 class=\"pull-left title\">{{SortAndFilterService.getSelectedLabel()}}&nbsp;<span>({{SortAndFilterService.getCount()}})</span><span ng-click=\"toggle()\" class=\"filtertoggle\"><icon type=\"caret-square-o-down\"></icon></span></h4>\n" +
     "\n" +
-    "\t<h4 class=\"pull-left title\">{{getSelectedLabel()}}&nbsp;<span>({{getCount()}})</span></h4>\r" +
-    "\n" +
-    "\r" +
-    "\n" +
-    "\t<h4 class=\"pull-left title\">{{SortAndFilterService.getSelectedLabel()}}&nbsp;<span>({{SortAndFilterService.getCount()}})</span><icon type=\"sort-desc\"></icon></h4>\r" +
-    "\n" +
-    "\t<div class=\"filterDropdown\">\r" +
-    "\n" +
-    "\t\t<span class=\"selected\">All Projects</span>\r" +
-    "\n" +
-    "\t\t<span>Favorites</span>\r" +
-    "\n" +
-    "\t\t<span>Created by Me</span>\r" +
-    "\n" +
-    "\t\t<span>I Can Edit</span>\r" +
-    "\n" +
-    "\t</div>\r" +
-    "\n" +
-    "\r" +
-    "\n" +
-    "\t<span>\r" +
-    "\n" +
-    "\t\t<icon type=\"filter\"></icon>\r" +
-    "\n" +
-    "\t\t<input type=\"text\" class=\"search-input\" ng-model=\"SortAndFilterService.searchText\" ng-change=\"SortAndFilterService.filter()\" placeholder=\"Filter Search\"/>&nbsp;\r" +
-    "\n" +
-    "\t</span>\r" +
-    "\n" +
-    "\t<div class=\"button-holder\">\r" +
-    "\n" +
-    "\t\t<div class=\"actions\">\r" +
-    "\n" +
-    "\t\t\t<span ng-if=\"view.create\" ng-click=\"create(CONFIG.displayActionsCreate)\"><icon type=\"plus\"></icon>CREATE</span>\r" +
-    "\n" +
-    "\t\t\t<!-- <span ng-if=\"view.filter\"><icon type=\"filter\"></icon></span>\r" +
-    "\n" +
-    "\t\t\t<span ng-if=\"view.search\"><search></search></span> -->\r" +
-    "\n" +
-    "\t\t</div>\r" +
-    "\n" +
-    "\t\t\r" +
-    "\n" +
-    "\t</div>\r" +
-    "\n" +
-    "</div>\r" +
-    "\n"
+    "\t<ul ms-link-group firstSelected=\"{{CONFIG.filterMenu.items[CONFIG.filterMenu.firstSelected].label}}\" radio=\"true\" class='filterDropdown dropshadow hide menu'>\n" +
+    "\t\t<li ng-repeat=\"item in CONFIG.filterMenu.items\" class=\"header\" ng-class=\"{selected: item.label === selectedItem}\">\n" +
+    "\t\t\t <a ms-link=\"{{item.label}}\" ng-click=\"setFilter(item.filterType, item, true)\">{{item.label}}</a>\n" +
+    "\t\t</li>\n" +
+    "\t\t<li>I Can Edit</li>\n" +
+    "    </ul>\n" +
+    "\t<span>\n" +
+    "\t\t<icon type=\"filter\"></icon>\n" +
+    "\t\t<input type=\"text\" class=\"search-input\" ng-model=\"SortAndFilterService.searchText\" ng-change=\"SortAndFilterService.filter()\" placeholder=\"Filter Search\"/>&nbsp;\n" +
+    "\t</span>\n" +
+    "\t<div class=\"button-holder\">\n" +
+    "\t\t<div class=\"actions\">\n" +
+    "\t\t\t<span ng-if=\"view.create\" ng-click=\"create(CONFIG.displayActionsCreate)\"><icon type=\"plus\"></icon>CREATE</span>\n" +
+    "\t\t\t<!-- <span ng-if=\"view.filter\"><icon type=\"filter\"></icon></span>\n" +
+    "\t\t\t<span ng-if=\"view.search\"><search></search></span> -->\n" +
+    "\t\t</div>\n" +
+    "\t\t\n" +
+    "\t</div>\n" +
+    "</div>\n"
   );
 
 
   $templateCache.put('views/directives/inline_description.tpl.html',
-    "<form class=\"inlineDescription\" name=\"inlineDescription\">\r" +
+    "<form class=\"inlineDescription\" name=\"inlineDescription\">\n" +
+    "\t<span class='field-label'>Description</span>\n" +
+    "\t<a ng-hide=\"isActive\" class='edit'ng-click=\"action()\"><icon  type=\"pencil\"></icon></a>\n" +
     "\n" +
-    "\t<span class='field-label'>Description</span>\r" +
+    "\t<span ng-show=\"isActive\" class=\"controls\"><button ng-click=\"submit(item)\" ng-disabled=\"(inlineDescription.$dirty && inlineDescription.$invalid) || inlineDescription.$pristine\"><icon type=\"check\"></icon></button>&nbsp;<button ng-click=\"cancel()\"><icon type=\"times\"></icon></button></span>\n" +
     "\n" +
-    "\t<a ng-hide=\"isActive\" class='edit'ng-click=\"action()\"><icon  type=\"pencil\"></icon></a>\r" +
-    "\n" +
-    "\r" +
-    "\n" +
-    "\t<span ng-show=\"isActive\" class=\"controls\"><button ng-click=\"submit(item)\" ng-disabled=\"(inlineDescription.$dirty && inlineDescription.$invalid) || inlineDescription.$pristine\"><icon type=\"check\"></icon></button>&nbsp;<button ng-click=\"cancel()\"><icon type=\"times\"></icon></button></span>\r" +
-    "\n" +
-    "\r" +
-    "\n" +
-    "\t<textarea ng-disabled=\"!isActive\" ng-maxlength=\"256\" ng-pattern='/^[^\\\\\\/\\?\\:\\*\"><|]+$/' ng-model=\"item.description\" class=\"description inputTarget\"></textarea>\r" +
-    "\n" +
+    "\t<textarea ng-disabled=\"!isActive\" ng-maxlength=\"256\" ng-pattern='/^[^\\\\\\/\\?\\:\\*\"><|]+$/' ng-model=\"item.description\" class=\"description inputTarget\"></textarea>\n" +
     "</form>"
   );
 
 
   $templateCache.put('views/directives/inline_rename.tpl.html',
-    "<form class=\"inlineRename\" name=\"rename\" novalidate role=\"form\">\r" +
-    "\n" +
-    "\t<span ng-transclude></span>\r" +
-    "\n" +
-    "\t<h4 class=\"title\" ng-hide=\"isActive\">{{item.title}}</h4>&nbsp;\r" +
-    "\n" +
-    "\t<a class=\"edit\" ng-click=\"action()\"><icon ng-hide=\"isActive\" type=\"pencil\" cname=\"pencil clearfix\"></icon></a>\r" +
-    "\n" +
-    "    <h4 ng-show=\"isActive\" class=\"title\">\r" +
-    "\n" +
-    "    \t<input type=\"text\" class=\"inputTarget\" ng-model=\"item.title\" required ng-maxlength=\"256\" ng-minlength=\"2\" ng-pattern='inputRestrictions.characterRestrictions' tabindex=\"1\" />&nbsp;\r" +
-    "\n" +
-    "    \t<button ng-click=\"submit(item)\" ng-disabled=\"(rename.$dirty && rename.$invalid) || rename.$pristine\"><icon type=\"check\"></icon></button>&nbsp;\r" +
-    "\n" +
-    "    \t<button ng-click=\"cancel()\"><icon type=\"times\"></icon></button>\r" +
-    "\n" +
-    "    </h4>\r" +
-    "\n" +
+    "<form class=\"inlineRename\" name=\"rename\" novalidate role=\"form\">\n" +
+    "\t<span ng-transclude></span>\n" +
+    "\t<h4 class=\"title\" ng-hide=\"isActive\">{{item.title}}</h4>&nbsp;\n" +
+    "\t<a class=\"edit\" ng-click=\"action()\"><icon ng-hide=\"isActive\" type=\"pencil\" cname=\"pencil clearfix\"></icon></a>\n" +
+    "    <h4 ng-show=\"isActive\" class=\"title\">\n" +
+    "    \t<input type=\"text\" class=\"inputTarget\" ng-model=\"item.title\" required ng-maxlength=\"256\" ng-minlength=\"2\" ng-pattern='inputRestrictions.characterRestrictions' tabindex=\"1\" />&nbsp;\n" +
+    "    \t<button ng-click=\"submit(item)\" ng-disabled=\"(rename.$dirty && rename.$invalid) || rename.$pristine\"><icon type=\"check\"></icon></button>&nbsp;\n" +
+    "    \t<button ng-click=\"cancel()\"><icon type=\"times\"></icon></button>\n" +
+    "    </h4>\n" +
     "</form>"
   );
 
 
   $templateCache.put('views/directives/ms_dropdown.tpl.html',
-    "<div class=\"ms-dropdown\" id=\"{{id}}\"> \r" +
-    "\n" +
-    "\t<h6 class=\"ms-label\" ng-class=\"{active: DropdownService.isActive(id)}\"><span ng-show=\"filterBy\"><icon type=\"filter\" cname=\"filter-icon\"></icon></span><span ng-click=\"select(selectedItem)\" class=\"status select\">{{selectedItem.label}}</span>&nbsp<span ng-click=\"toggle()\" class=\"toggle\"><icon type=\"caret-square-o-down\"></icon></span></h6> \r" +
-    "\n" +
-    "\t<ul class=\"ms-select-list dropshadow hide\"> \r" +
-    "\n" +
-    "\t\t<li class=\"ms-item selectSort\" ng-repeat=\"item in items\" ng-class=\"{disabled:item.label === selectedItem.label}\" ng-click=\"selectSort(item)\"><icon type=\"check\" cname=\"ms-ok\"></icon>{{item.label}}</li> \r" +
-    "\n" +
-    "\t\t<ul ng-if=\"selectedItem.filters\"> \r" +
-    "\n" +
-    "\t\t\t<li class=\"divider\"></li> \r" +
-    "\n" +
-    "\t\t\t<li class=\"ms-sublabel\">FILTER</li> \r" +
-    "\n" +
-    "\t\t\t<li ng-repeat=\"filter in selectedItem.filters\" class=\"ms-item selectFilter\" ng-click=\"selectFilter(filter)\" ng-class=\"{selected:selectedFilter === filter}\"><icon type=\"check\" cname=\"ms-ok\"></icon>{{filter.label}}</li> \r" +
-    "\n" +
-    "\t\t\t<li ng-if=\"selectedItem.template\"><ng-include src=\"selectedItem.template\"></ng-include></li> \r" +
-    "\n" +
-    "\t\t</ul> \r" +
-    "\n" +
-    "\t</ul> \r" +
-    "\n" +
+    "<div class=\"ms-dropdown\" id=\"{{id}}\"> \n" +
+    "\t<h6 class=\"ms-label\" ng-class=\"{active: DropdownService.isActive(id)}\"><span ng-show=\"filterBy\"><icon type=\"filter\" cname=\"filter-icon\"></icon></span><span ng-click=\"select(selectedItem)\" class=\"status select\">{{selectedItem.label}}</span>&nbsp<span ng-click=\"toggle()\" class=\"toggle\"><icon type=\"caret-square-o-down\"></icon></span></h6> \n" +
+    "\t<ul class=\"ms-select-list dropshadow hide\"> \n" +
+    "\t\t<li class=\"ms-item selectSort\" ng-repeat=\"item in items\" ng-class=\"{disabled:item.label === selectedItem.label}\" ng-click=\"selectSort(item)\"><icon type=\"check\" cname=\"ms-ok\"></icon>{{item.label}}</li> \n" +
+    "\t\t<ul ng-if=\"selectedItem.filters\"> \n" +
+    "\t\t\t<li class=\"divider\"></li> \n" +
+    "\t\t\t<li class=\"ms-sublabel\">FILTER</li> \n" +
+    "\t\t\t<li ng-repeat=\"filter in selectedItem.filters\" class=\"ms-item selectFilter\" ng-click=\"selectFilter(filter)\" ng-class=\"{selected:selectedFilter === filter}\"><icon type=\"check\" cname=\"ms-ok\"></icon>{{filter.label}}</li> \n" +
+    "\t\t\t<li ng-if=\"selectedItem.template\"><ng-include src=\"selectedItem.template\"></ng-include></li> \n" +
+    "\t\t</ul> \n" +
+    "\t</ul> \n" +
     "</div>"
   );
 
 
   $templateCache.put('views/directives/name.tpl.html',
-    "<li class=\"ms-holder\">\r" +
-    "\n" +
-    "\t<input type=\"text\" class=\"ms-name-input\" ng-model=\"name\" placeholder=\"Enter Name\" ng-disabled=\"enabledOn(selectedFilter)\" ng-click=\"dontPassEvent($event)\"/>\r" +
-    "\n" +
-    "</li>\r" +
-    "\n" +
-    "<li class=\"ms-holder\">\r" +
-    "\n" +
-    "\t<button class=\"btn btn-primary ms-apply submit-button\" ng-disabled=\"!name || enabledOn(selectedFilter)\" ng-click=\"submit(name)\">apply</button>\r" +
-    "\n" +
-    "</li>\r" +
-    "\n"
+    "<li class=\"ms-holder\">\n" +
+    "\t<input type=\"text\" class=\"ms-name-input\" ng-model=\"name\" placeholder=\"Enter Name\" ng-disabled=\"enabledOn(selectedFilter)\" ng-click=\"dontPassEvent($event)\"/>\n" +
+    "</li>\n" +
+    "<li class=\"ms-holder\">\n" +
+    "\t<button class=\"btn btn-primary ms-apply submit-button\" ng-disabled=\"!name || enabledOn(selectedFilter)\" ng-click=\"submit(name)\">apply</button>\n" +
+    "</li>\n"
   );
 
 
   $templateCache.put('views/directives/sortable_columns.tpl.html',
-    "<div ng-switch on=\"displayBy\" class=\"text-holder\"> \r" +
-    "\n" +
-    "\t<span ng-switch-when=\"Last Modified\" bind-once>{{item.modifiedOn | timeago }}</span> \r" +
-    "\n" +
-    "\t<span ng-switch-when=\"Modified By\" bind-once>{{item.modifiedBy}}</span> \r" +
-    "\n" +
-    "\t<span ng-switch-when=\"Type\" bind-once>{{item.type}}</span> \r" +
-    "\n" +
-    "\t<span ng-switch-when=\"Creator\" bind-once>{{item.createdBy}}</span> \r" +
-    "\n" +
-    "\t<span ng-switch-when=\"Created Date\" bind-once>{{item.createdOn | date: 'longDate' }}</span> \r" +
-    "\n" +
-    "\t<span ng-switch-default>FAIL</span> \r" +
-    "\n" +
+    "<div ng-switch on=\"displayBy\" class=\"text-holder\"> \n" +
+    "\t<span ng-switch-when=\"Last Modified\" bind-once>{{item.modifiedOn | timeago }}</span> \n" +
+    "\t<span ng-switch-when=\"Modified By\" bind-once>{{item.modifiedBy}}</span> \n" +
+    "\t<span ng-switch-when=\"Type\" bind-once>{{item.type}}</span> \n" +
+    "\t<span ng-switch-when=\"Creator\" bind-once>{{item.createdBy}}</span> \n" +
+    "\t<span ng-switch-when=\"Created Date\" bind-once>{{item.createdOn | date: 'longDate' }}</span> \n" +
+    "\t<span ng-switch-default>FAIL</span> \n" +
     "</div>"
   );
 
 
   $templateCache.put('views/directives/sorting_options.tpl.html',
-    "<div class=\"{{label}} heading\" ng-class=\"{'active': SortAndFilterService.isActive(label)}\">\r" +
-    "\n" +
-    "\t<a ng-click=\"sort($event, label)\" ng-bind=\"display\"></a>&nbsp;\r" +
-    "\n" +
+    "<div class=\"{{label}} heading\" ng-class=\"{'active': SortAndFilterService.isActive(label)}\">\n" +
+    "\t<a ng-click=\"sort($event, label)\" ng-bind=\"display\"></a>&nbsp;\n" +
     "</div> "
   );
 
