d4b91daa8bdbfe0d8f816fd04df9f8d4cbaaa275
Mark Gable <mgable@marketshare.com>
2014-11-06 10:52:23 -0800

    writing e2e tests

--- ThreeSixtyOneView/test/e2e/test.js
+++ ThreeSixtyOneView/test/e2e/test.js
@@ -1,7 +1,7 @@
 // spec.js
 "use strict";
 
-var projectUrl = 'http://127.0.0.1:9001/#/projects';
+var projectUrl = '/#/projects';
 
 describe('Project Listing', function() {
 	var hasClass = function (element, cls) {
@@ -14,27 +14,27 @@ describe('Project Listing', function() {
 		function(){browser.get(projectUrl);}
 	)
 
-	xit('should have a title', function() {
+	it('should have a title', function() {
 		expect(browser.getTitle()).toEqual('360 One View');
 	});
 
-	xit ('should list projects', function(){
+	it ('should list projects', function(){
 		expect(element.all(by.repeater('item in getData()')).count()).toBeGreaterThan(0);
 	});
 
-	xit("should search", function(){
+	it("should search", function(){
 		element(by.model('SortAndFilterService.searchText')).sendKeys('master project');
 		expect(element.all(by.repeater('item in getData()')).count()).toBe(1);
 	});
 
-	xit ("should toggle the filter menu dropdown", function(){
+	it ("should toggle the filter menu dropdown", function(){
 		var elem = element(by.css('.filterDropdown'));
 		expect(hasClass(elem, 'hide')).toBe(true);
 		element(by.css(".filtertoggle")).click();
 		expect(hasClass(elem, 'hide')).toBe(false);
 	});
 
-	xit("should order by name", function(){
+	it("should order by name", function(){
 		var elem = element(by.css('[ng-bind="display"]')),
 			titles = element.all(by.repeater('item in getData()').column('title'));
 		
@@ -56,13 +56,51 @@ describe('Project Listing', function() {
 
 	});
 
-	xit('should present the total viewable items', function(){
+	it('should present the total viewable items', function(){
 		var itemCount = element(by.css('.display-actions h4.title span:first-child'));
 		expect(itemCount.getText()).toContain(element.all(by.repeater('item in getData()')).count());
 	});
 
+	describe("Sorter", function(){
+		it("should have at least one project", function(){
+			expect(element.all(by.repeater('item in getData()')).count()).toBeGreaterThan(0);
+		});
 
-	describe("Favorite behaviors", function(){
+		it("should have a master project", function(){
+			expect(element.all(by.css(".master")).count()).toBe(1);
+		});
+
+		it("should have an active selection", function(){
+			var first = element.all(by.repeater('item in getData()')).first();
+			expect(hasClass(first, "active")).toBe(true);
+		});
+
+		it("should show the number of projects", function(){
+			var itemCount = element(by.css('.display-actions h4.title span:first-child'));
+			expect(itemCount.getText()).toContain(element.all(by.repeater('item in getData()')).count());
+		});
+
+		it("should change the active selection on click", function(){
+			var first = element.all(by.repeater('item in getData()')).first(),
+			last = element.all(by.repeater('item in getData()')).last();
+			expect(hasClass(first, "active")).toBe(true);
+			expect(hasClass(last, "active")).toBe(false);
+			last.click();
+			expect(hasClass(first, "active")).toBe(false);
+			expect(hasClass(last, "active")).toBe(true);
+			first.click();
+			expect(hasClass(first, "active")).toBe(true);
+			expect(hasClass(last, "active")).toBe(false);
+		});
+
+		it("should redirect to Project Dashboard when a project is clicked", function(){
+			var projectName = element.all(by.repeater('item in getData()')).first().element(by.css('.default.ng-binding'));
+			projectName.click();
+			expect(browser.getLocationAbsUrl()).toContain("/#/dashboard/");
+		});
+	});
+
+	xdescribe("Favorite behaviors", function(){
 		it("should favorite the master project", function(){
 			expect(element.all(by.css(".master .favorite")).count()).toBe(1);
 		});
@@ -91,7 +129,6 @@ describe('Project Listing', function() {
 				expect(hasClass(firstElement, 'favorite')).not.toBe(isFavorite);
 			});
 		});
-
 
 		it("should filter by favorite", function(){
 			var filteredCount, unFilteredCount,
