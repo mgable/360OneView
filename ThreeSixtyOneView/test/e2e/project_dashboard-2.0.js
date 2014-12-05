49d706fe6119d767679c1f53d058d02b11c9b65d
Mark Gable <mgable@marketshare.com>
2014-12-04 17:05:18 -0800

    jiraId:MAR-456 e2e tests

--- ThreeSixtyOneView/test/e2e/project_dashboard-2.0.js
+++ ThreeSixtyOneView/test/e2e/project_dashboard-2.0.js
@@ -4,7 +4,7 @@ var projectUrl = '/#/projects?e2e=true',
 	dashboardUrl = '/#/dashboard/:id?e2e=true';
 
 	//TEMP data - remove in production
-	var projectId = "11cb16491d5031989322c37003b971d8";
+	var projectId = "3626699b10323fa0a7fa9aa35876bd82";
 	dashboardUrl = dashboardUrl.replace(/:id/, projectId);
 
 describe('Project Dashboard', function() {
@@ -12,7 +12,7 @@ describe('Project Dashboard', function() {
 	    return element.getAttribute('class').then(function (classes) {
 	        return classes.split(' ').indexOf(cls) !== -1;
 	    });
-	}, menuId, nameField, column_1, createdBy, ascending, descending, ascendingButton, descendingButton, dropdown, nameButton, column_1Button, createdByButton, nameLabel, nameLabelField, dropdownButton, column_1Label, column_1LabelField, projectId;
+	}, menuId, nameField, column_1, createdBy, ascending, descending, ascendingButton, descendingButton, dropdown, nameButton, column_1Button, createdByButton, nameLabel, nameLabelField, dropdownButton, column_1Label, column_1LabelField, projectId, testFileName = "My New Test Project- " + Date.now();
 
 	beforeEach(
 		function(){
@@ -47,8 +47,7 @@ describe('Project Dashboard', function() {
 			submitButton,
 			cancelButton, 
 			firstItemTitle,
-			noScenariosAlert,
-			testFileName = "My New Test Project- " + Date.now();
+			noScenariosAlert;
 
 		browser.get(projectUrl);
 		createButton.click();
@@ -75,7 +74,8 @@ describe('Project Dashboard', function() {
 	});
 
 	describe("Scenario List", function(){
-		var testScenarionName = "My new test scenario title - " + Date.now(),
+		var testScenarionNameFirst = "My FIRST new test scenario title - " + Date.now(),
+			testScenarionNameSecond = "My SECOND new test scenario title - " + Date.now(),
 			testScenarionDescription = "My new test scenario description.",
 			noScenarios = "//a[@data-ms-id='noScenariosAlert']", 
 			noScenariosAlert = element(by.xpath(noScenarios)),
@@ -100,21 +100,26 @@ describe('Project Dashboard', function() {
 			browser.get(dashboardUrl);
 		});
 
+		xit("should have an active selection", function(){
+			var first = element.all(by.repeater('item in getData()')).first();
+			expect(hasClass(first, "active")).toBe(true);
+		});
+
 		xit("should have no scenarios at time of creation", function(){
 			var data = element.all(by.repeater('item in getData()')),
 				itemCount = element(by.xpath("//span[@data-ms-id='dataCount']"));
 			expect(data.count()).toBe(0);
 			expect(itemCount.getText()).toContain(data.count());
 		});
-
-		xit ("should display the create scenario alert", function(){
-			expect(noScenariosAlert.isPresent()).toBe(true)
+		
+		xit("should display the create scenario alert", function(){
+			expect(noScenariosAlert.isPresent()).toBe(true);
 		});
 
 		xit("should open the create new scenario dialog box and create a new scenario", function(){
 			noScenariosAlert.click();
 			browser.waitForAngular();
-			inputName.sendKeys(testScenarionName);
+			inputName.sendKeys(testScenarionNameFirst);
 			inputDescription.sendKeys(testScenarionDescription);
 			submitButton.click();
 			browser.waitForAngular();
@@ -129,7 +134,7 @@ describe('Project Dashboard', function() {
 			expect(itemCount.getText()).toContain(data.count());
 			expect(noScenariosAlert.isPresent()).toBe(false);
 			title.first().getText().then(function(text){
-				expect(text).toBe(testScenarionName);
+				expect(text).toBe(testScenarionNameFirst);
 			})
 		});
 
@@ -143,7 +148,7 @@ describe('Project Dashboard', function() {
 			createButton.click();
 			browser.waitForAngular();
 			expect(submitButton.isEnabled()).toBe(false);
-			inputName.sendKeys("New " + testScenarionName);
+			inputName.sendKeys("New " + testScenarionNameFirst);
 			expect(submitButton.isEnabled()).toBe(true);
 		});
 
@@ -151,7 +156,7 @@ describe('Project Dashboard', function() {
 			createButton.click();
 			browser.waitForAngular();
 			expect(submitButton.isEnabled()).toBe(false);
-			inputName.sendKeys(testScenarionName);
+			inputName.sendKeys(testScenarionNameFirst);
 			expect(submitButton.isEnabled()).toBe(false);
 		});
 
@@ -195,7 +200,7 @@ describe('Project Dashboard', function() {
 			expect(submitButton.isEnabled()).toBe(true);
 		});
 
-		it("should copy a scenario", function(){
+		xit("should copy a scenario", function(){
 			var scenarioElement = element(by.repeater('item in getData()').row(0).column('title')),
 				scenario = element.all(by.repeater('item in getData()').row(0)),
 				data = element.all(by.repeater('item in getData()')),
@@ -211,6 +216,8 @@ describe('Project Dashboard', function() {
 				scenarioElement.getText().then(function(scenarioTitle){
 					modalInput.getAttribute("value").then(function(inputText){
 						expect("COPY -- " + scenarioTitle).toEqual(inputText);
+						modalInput.clear();
+						modalInput.sendKeys(testScenarionNameSecond);
 					});
 				});
 				submitButton.click();
@@ -226,8 +233,156 @@ describe('Project Dashboard', function() {
 					});
 				});
 			});
+		});
 
-			
+		xit("should search scenarios", function(){
+			element(by.model('SortAndFilterService.searchText')).sendKeys('FIRST');
+			expect(element.all(by.repeater('item in getData()')).count()).toBe(1);
+		});
+
+		xdescribe("Sort functions: ", function(){
+			beforeEach(
+				function(){
+					dropdown = "//span[@data-ms-id='toggle_column_1']",
+					nameField = "//a[@data-ms-id='name-field']",
+					nameLabel = "//div[@data-ms-id='name-label']",
+					column_1 = "//span[@data-ms-id='select_column_1']",
+					column_1Label = "//h6[@data-ms-id='label_column_1']",
+					createdBy = "//li[@data-ms-id='Created Date']",
+					ascending = "//li[@data-ms-id='ascending']",
+					descending = "//li[@data-ms-id='descending']",
+					ascendingButton = element(by.xpath(ascending)),
+					descendingButton = element(by.xpath(descending)),
+					dropdownButton = element(by.xpath(dropdown)),
+					nameButton = element(by.xpath(nameField)),
+					nameLabelField = element(by.xpath(nameLabel)),
+					column_1Button = element(by.xpath(column_1)),
+					createdByButton = element(by.xpath(createdBy)),
+					column_1LabelField = element(by.xpath(column_1Label));
+				}
+			);
+
+			it("should switch between ordering by name, modified last and created on", function(){
+				var data;
+
+				nameButton.click();
+				data = element.all(by.repeater('item in getData()').column('title'));
+				expect(data.first().getText()).toBeLessThan(data.last().getText());
+				expect(hasClass(nameLabelField, 'active')).toBe(true);
+				expect(hasClass(column_1LabelField, 'active')).toBe(false);
+
+				column_1Button.click();
+				data = element.all(by.repeater('item in getData()').column('modifiedOn'));
+				expect(data.first().getText()).toBeGreaterThan(data.last().getText());
+				expect(hasClass(nameLabelField, 'active')).toBe(false);
+				expect(hasClass(column_1LabelField, 'active')).toBe(true);
+
+				dropdownButton.click()
+				createdByButton.click();
+
+				data = element.all(by.repeater('item in getData()').column('createdOn'));
+				expect(data.first().getText()).toBeGreaterThan(data.last().getText());
+				expect(hasClass(nameLabelField, 'active')).toBe(false);
+				expect(hasClass(column_1LabelField, 'active')).toBe(true);
+			});
+
+			it("should order by name", function(){
+				var elem = element(by.xpath(nameField)),
+					titles = element.all(by.repeater('item in getData()').column('title'));
+				
+				elem.click();
+				
+				titles.first().getText().then(function(firstText){
+					titles.last().getText().then(function(lastText){
+						expect(firstText.toLowerCase()).toBeLessThan(lastText.toLowerCase());
+					});
+				});
+
+				elem.click();
+
+				titles.first().getText().then(function(firstText){
+					titles.last().getText().then(function(lastText){
+						expect(lastText.toLowerCase()).toBeLessThan(firstText.toLowerCase());
+					});
+				});
+			});
+
+			it("should sort by last modified", function(){
+				var dates = element.all(by.repeater('item in getData()').column('modifiedOn'));
+				dates.first().getText().then(function(firstDate){
+					dates.last().getText().then(function(lastDate){
+						expect(firstDate).toBeGreaterThan(lastDate);
+					});
+				});
+
+				dropdownButton.click();
+				ascendingButton.click();
+
+				dates.first().getText().then(function(firstDate){
+					dates.last().getText().then(function(lastDate){
+						expect(firstDate).toBeLessThan(lastDate);
+					});
+				});
+			});
+
+			it("should order by created date", function(){
+				var dates = element.all(by.repeater('item in getData()').column('createdOn'));
+
+				dropdownButton.click();
+				createdByButton.click();
+
+				dates.first().getText().then(function(firstDate){
+					dates.last().getText().then(function(lastDate){
+						expect(firstDate).toBeGreaterThan(lastDate);
+					});
+				});
+
+				dropdownButton.click();
+				ascendingButton.click();
+
+				dates.first().getText().then(function(firstDate){
+					dates.last().getText().then(function(lastDate){
+						expect(firstDate).toBeLessThan(lastDate);
+					});
+				});
+			});
+		});
+
+		xdescribe("Favorite behaviors", function(){
+
+			it("should toggle favorite", function(){
+				var firstElement = element.all(by.css(".favorites a")).first(),
+					isFavorite;
+					
+				hasClass(firstElement, 'favorite').then(function(data){
+					isFavorite = data;
+					firstElement.click();
+					expect(hasClass(firstElement, 'favorite')).not.toBe(isFavorite);
+				});
+			});
+
+			it("should save the favorite", function(){
+				var firstElement = element.all(by.css(".favorites a")).first(),
+					isFavorite;
+					
+				hasClass(firstElement, 'favorite').then(function(data){
+					isFavorite = data;
+					firstElement.click();
+					expect(hasClass(firstElement, 'favorite')).not.toBe(isFavorite);
+					browser.get(dashboardUrl);
+					firstElement = element.all(by.css(".favorites a")).first();
+					expect(hasClass(firstElement, 'favorite')).not.toBe(isFavorite);
+				});
+			});
+		});
+
+		xdescribe("Breadcrumbs", function(){
+			var breadcrumb = ".breadcrumbs span",
+				breadcrumbField = element(by.css(breadcrumb));
+
+			it("should have the correct label", function(){
+				expect(breadcrumbField.getText()).toEqual("ALL PROJECTS > " + testFileName.toUpperCase());
+			});
 		});
 	});
 });
