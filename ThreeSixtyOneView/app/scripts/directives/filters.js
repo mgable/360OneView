80e6af5f01cc8467df8ab23f95b8cd252e5fcf29
Mark Gable <mgable@marketshare.com>
2014-12-15 15:21:58 -0800

    JiraID:MAR-456

--- ThreeSixtyOneView/app/scripts/directives/filters.js
+++ ThreeSixtyOneView/app/scripts/directives/filters.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//NOT USED!!!!!
+
 /**
  * @ngdoc directive
  * @name ThreeSixtyOneView.directive:filters
@@ -16,9 +18,16 @@ angular.module('ThreeSixtyOneView.directives').directive('filters', function() {
 			category: '=',
 			expandall: '='
 		},
-		template: '<div class="pbFilterList"><member ng-repeat="member in collection.members | orderBy:\'label\':sortReverse" member="member" filters="filters" category="category" sortOrder="sortReverse" expanded="expanded" expandall="expandall"></member></div>',
+		template: '<div class="pbFilterList"><member ng-repeat="member in collection.members | orderBy:\'label\'" member="member" filters="filters" category="category"  expanded="expanded" expandall="expandall"></member></div>',
 		link: function(scope) {
 			scope.expanded = {};
 		}
 	};
 });
+
+
+// <filters ng-if="searchResults.members" collection="searchResults" filters="addedFilter" category="{label: selectedFilter.cat.label}" expandall="filterSearch"></filters>
+
+// <div class="pbFilterList" ng-if="searchResults.members">
+// <member ng-repeat="member in searchResults.members | orderBy:\'label\'" member="member" filters="addedFilter" category="{label: selectedFilter.cat.label}"  expanded="{}" expandall="filterSearch"></member>
+// </div>
--- ThreeSixtyOneView/app/scripts/directives/member.js
+++ ThreeSixtyOneView/app/scripts/directives/member.js
@@ -6,20 +6,20 @@
  * @description
  * # member
  */
-angular.module('ThreeSixtyOneView.directives').directive('member', ['$compile', function($compile) {
+angular.module('ThreeSixtyOneView.directives').directive('member', ['$compile', "$rootScope", function($compile, $rootScope) {
 	return {
 		restrict: 'E',
 		replace: true,
 		scope: {
-			member: '=',
-			filters: '=',
-			category: '=',
-			expanded: '=',
-			sortOrder: '=',
-			expandall: '='
+			member: '=', // <object> {label: <dimension>, members: []} individual filter dimension i.e. touchpoint (magazine, newspaper, radio), nameplate (brand, car truck), region (New York, Miami, Seattle), time (August 2014, September 2014)
+			filters: '=', // <object> {touchpoint:{}, nameplate: {}, Region: {}, Time: {}, KPI: {}} summation of all members
+			category: '=', // <object> {label: <filter dimention>} currently selected filter dimension (time, touchpoint, region, nameplate)
+			expanded: '=', // empty || <object> {<filter dimenstion>: true, <filter dimenstion>: true} all expanded filters
+			expandall: '=' // <object> {label: <search text> } object containing search text
 		},
-		templateUrl: 'views/directives/members.tpl.html',
+		templateUrl: 'views/directives/member.tpl.html',
 		link: function(scope, element) {
+
 			var modifyItems = function(member, add) {
 				if(member.members.length > 0) {
 					for(var i = 0; i < member.members.length; i++) {
@@ -27,25 +27,15 @@ angular.module('ThreeSixtyOneView.directives').directive('member', ['$compile', 
 					}
 				} else {
 					scope.filters[scope.category.label][member.label] = add;
+					$rootScope.$broadcast("FOO", scope.category);
 				}
-			};
-
-			scope.toggleMember = function(member) {
-				var checkedItems = scope.checkedItems(member);
-
-				if(checkedItems.checked < checkedItems.total) {
-					modifyItems(member, true);
-				} else {
-					modifyItems(member, false);
-				}
-			};
-
-			scope.checkedItems = function(member) {
+			},
+			checkedItems = function(member) {
 				var output = {checked: 0, total: 0};
 
 				if(member.members.length > 0) {
 					for(var i = 0; i < member.members.length; i++) {
-						var tempOutput = scope.checkedItems(member.members[i]);
+						var tempOutput = checkedItems(member.members[i]);
 						output.checked += tempOutput.checked;
 						output.total += tempOutput.total;
 					}
@@ -59,18 +49,41 @@ angular.module('ThreeSixtyOneView.directives').directive('member', ['$compile', 
 				return output;
 			};
 
+			scope.expanded = scope.expanded || {};
+
+			scope.toggleMember = function(member) {
+				var item = checkedItems(member);
+
+				if(item.checked < item.total) {
+					modifyItems(member, true);
+				} else {
+					modifyItems(member, false);
+				}
+			};
+
+
 			scope.determineStyle = function(member){
-				return scope.checkedItems(member).checked / scope.checkedItems(member).total;
+				return (checkedItems(member).checked / checkedItems(member).total);
+			};
+
+			scope.outputSelectedOverTotal = function(member){
+				return checkedItems(member).checked.toString() + "/" + checkedItems(member).total.toString();
 			};
 
 			scope.hasMembers = function(){
-				return scope.member.members.length > 0
-			}
+				return scope.member.members.length > 0;
+			};
 
-			scope.numCheckedItems = scope.checkedItems(scope.member);
+			scope.setToggleStyle = function(member){
+				return (!!scope.expanded[member.label] || scope.expandall.label !== '') ? 'fa-rotate-90':''
+			};
+
+			scope.isAllSelected = function(member){
+				return scope.determineStyle(member) === 1;
+			};
 
 			if(scope.member.members.length > 0) {
-				$compile('<div class="pbFilterCollection" ng-class="{pbFilterCollapsed: !expanded[member.label] && expandall.label === \'\'}"><member ng-repeat="child in member.members | orderBy:\'label\':false" member="child" filters="filters" category="category" sortOrder="sortOrder" expanded="expanded" expandall="expandall"></member></div>')(scope, function(cloned) {
+				$compile('<div class="pbFilterCollection" ng-class="{pbFilterCollapsed: !expanded[member.label] && expandall.label === \'\'}"><member ng-repeat="child in member.members | orderBy:\'label\':false" member="child" filters="filters" category="category" expanded="expanded" expandall="expandall"></member></div>')(scope, function(cloned) {
 					element.after(cloned);
 				});
 			}
