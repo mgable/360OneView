2a7a971b7f815795315f0ba9823022561eb7dfb5
Mark Gable <mgable43@gmail.com>
2014-12-16 13:44:27 -0800

    jiraId:MAR-456

--- ThreeSixtyOneView/app/scripts/models/cube_model.js
+++ ThreeSixtyOneView/app/scripts/models/cube_model.js
@@ -6,32 +6,29 @@ angular.module('ThreeSixtyOneView.services')
 	 transformResponse = function(data){
 		var dimensions = [], response = JSON.parse(data);
 		if (response){
-			for(var i = 0; i < response.dimensions.length; i++) {
-				dimensions[i] = {};
-				dimensions[i].label = response.dimensions[i].label;
-				dimensions[i].id = response.dimensions[i].id;
-				dimensions[i].levels = [];
-				for(var j = 0; j < response.dimensions[i].hierarchies.length; j++) {
-					for(var k = 0; k < response.dimensions[i].hierarchies[j].levels.length; k++) {
-						if(k === response.dimensions[i].hierarchies[j].levels.length -1) {
-							if(j === response.dimensions[i].hierarchies.length - 1) {
-								dimensions[i].levels.push({
-									dimension: {id: response.dimensions[i].id},
-									hierarchy: {id: response.dimensions[i].hierarchies[j].id},
-									level: {id: response.dimensions[i].hierarchies[j].levels[k].id,	label: response.dimensions[i].hierarchies[j].levels[k].label}
-								});
-							}
-						} else {
-							dimensions[i].levels.push({
-								dimension: {id: response.dimensions[i].id},
-								hierarchy: {id: response.dimensions[i].hierarchies[j].id},
-								level: {id: response.dimensions[i].hierarchies[j].levels[k].id,	label: response.dimensions[i].hierarchies[j].levels[k].label}
-							});
-						}
-						
-					}
-				}
-			}
+
+			_.each(response.dimensions, function(dimension){
+				var obj = {};
+				obj.label = dimension.label;
+				obj.id = dimension.id;
+				obj.levels = [];
+
+				_.each(dimension.hierarchies, function(hierarchy){
+
+					_.each(hierarchy.levels, function(level){
+
+						obj.levels.push({
+							dimension: {id: dimension.id},
+							hierarchy: {id: hierarchy.id},
+							level: {id: level.id,	label: level.label}
+						});
+
+					});
+				});
+
+				dimensions.push(obj);
+			});
+
 			return dimensions;
 		}
 		return data;
