b0c2eed6e5a4bf985395566bed5a0c2ad7c5eace
Mark Gable <mgable@marketshare.com>
2015-02-09 10:13:16 -0800

    merge

--- ThreeSixtyOneView/app/scripts/directives/expand_collapse_control.js
+++ ThreeSixtyOneView/app/scripts/directives/expand_collapse_control.js
@@ -10,18 +10,6 @@ angular.module('ThreeSixtyOneView.directives').directive("tabsControl", [functio
                     this.tabs.push(item);
                 };
 
-                element.on('click', function() {
-                    if(scope.$parent.tabCollapseStatus() === 'disable') {
-                        return;
-                    } else if(scope.$parent.tabCollapseStatus() === 'intermediate') {
-                        scope.$parent.tabCollapseStatus('enable');
-                        return;
-                    }
-
-                    scope.$parent.tabControl[tabId].collapsed = !scope.$parent.tabControl[tabId].collapsed;
-                    $(element).toggleClass('lightestgrayBg');
-                    $(targetArea).toggleClass('hidden');
-
                 this.closeAll = function(){
                     _.each(this.tabs, function(tab, index) {
                         $(tab.target).addClass('hidden');
@@ -40,6 +28,7 @@ angular.module('ThreeSixtyOneView.directives').directive("tabsControl", [functio
 
                 element.on('click', function(evt) {
                     var active = true;
+
                     if (!disabled){
                         if (!$(target).hasClass('hidden')){
                             active = false;
