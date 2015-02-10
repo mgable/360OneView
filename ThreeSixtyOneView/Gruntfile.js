42ed008384bc36bde0ae63968d40525e1789e7af
Mark Gable <mgable@marketshare.com>
2015-02-10 09:55:50 -0800

    fixing build issue

--- ThreeSixtyOneView/Gruntfile.js
+++ ThreeSixtyOneView/Gruntfile.js
@@ -371,7 +371,7 @@ module.exports = function(grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         '*.html',
-                        'views/{,*/}*.html',
+                        'views/**/{,*/}*.html',
                         'images/{,*/}*.{webp}',
                         'fonts/*',
                         'styles/vendor/*.css'
