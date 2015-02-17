a14281e2104f7d4cf2010a1ead1a524086d3558a
Behrooz <bkamali@marketshare.com>
2015-02-17 10:06:33 -0800

    jiraId:MAR-6134 refactor pivot model response transformer

--- ThreeSixtyOneView/app/scripts/models/pivot_model.js
+++ ThreeSixtyOneView/app/scripts/models/pivot_model.js
@@ -3,13 +3,58 @@
 angular.module('ThreeSixtyOneView.services')
   .factory('PivotModel', ["$location", "Resource", "CONFIG", "SERVER", function ($location, Resource, CONFIG, SERVER) {
 	var resource = new Resource(SERVER[$location.host()]  + CONFIG.application.api.pivotdata),
+		pivotTable,
+		// contains styling and formatting information
+		helperObject,
+		columnIndex,
+		getNumberOfRows = function(response) {
+			var output = !!response[0][0].key.value.coordinates.rowAddresses ? response[0][0].key.value.coordinates.rowAddresses.length : 1;
+			return output;
+		},
 		getNumberOfColumns = function(response) {
 			var output = !!response[0][0].key.value.coordinates.columnAddresses ? response[0][0].key.value.coordinates.columnAddresses.length : 1;
 			return output;
 		},
-		getNumberOfRows = function(response) {
-			var output = !!response[0][0].key.value.coordinates.rowAddresses ? response[0][0].key.value.coordinates.rowAddresses.length : 1;
-			return output;
+		getRowElements = function(row) {
+			return row.key.value.coordinates.rowAddresses;
+		},
+		getColumnElements = function(column) {
+			return column.key.value.coordinates.columnAddresses;
+		},
+		getElementLabel = function(element) {
+			return element.cellValue.specification.members[0].label;
+		},
+		initializeTransformOutputs = function(numRows, numCols) {
+			var i, j;
+			pivotTable = [];
+			helperObject = [];
+			for(i = 0; i < numCols; i++) {
+				pivotTable[i] = {};
+				helperObject[i] = {};
+				for(j = 0; j < numRows; j++) {
+					pivotTable[i][j] = '';
+				}
+			}
+		},
+		formPivotTable = function(tree, numCols, columnLabels) {
+			if(angular.isArray(tree)) {
+				_.each(tree, function(value, index) {
+					pivotTable[index + numCols][columnIndex] = value.value;
+					helperObject[index + numCols][columnIndex] = value.format;
+				});
+				_.each(columnLabels, function(columnLabel, index) {
+					pivotTable[index] = pivotTable[index] || {};
+					pivotTable[index][columnIndex] = columnLabel;
+					helperObject[index] = helperObject[index] || {};
+				});
+				columnIndex++;
+			} else if(angular.isObject(tree)) {
+				_.each(tree, function(branch, columnLabel) {
+					var newLabels = _.values(columnLabels);
+					newLabels.push(columnLabel);
+					formPivotTable(branch, numCols, newLabels);
+				});
+			}
 		},
 		transformResponse = function(data) {
 			if(data === '') {
@@ -19,33 +64,26 @@ angular.module('ThreeSixtyOneView.services')
 			var response = JSON.parse(data);
 			
 			if(response.length > 0) {
-				var i, j,
-					tableTree = {},
+				var tableTree = {},
 					// number of selected dimensions in columns
 					numCols = getNumberOfColumns(response),
 					// number of selected dimensions in rows
-					numRows = getNumberOfRows(response),
-					pivotTable = [],
-					columnIndex = numRows,
-					helperObject = [];
+					numRows = getNumberOfRows(response);
 
-				for(i = 0; i < numCols; i++) {
-					pivotTable[i] = {};
-					helperObject[i] = {};
-					for(j = 0; j < numRows; j++) {
-						pivotTable[i][j] = '';
-					}
-				}
+				// columns index start from row headers, e.g., if there are two row headers, first column is at index 2
+				columnIndex = numRows;
+
+				initializeTransformOutputs(numRows, numCols);
 
 				_.each(response, function(row, rowIndex) {
 					pivotTable[rowIndex + numCols] = {};
 					helperObject[rowIndex + numCols] = {};
-					_.each(row[0].key.value.coordinates.rowAddresses, function(rowElement, rowElementIndex) {
-						pivotTable[rowIndex + numCols][rowElementIndex] = rowElement.cellValue.specification.members[0].label;
+					_.each(getRowElements(row[0]), function(rowElement, rowElementIndex) {
+						pivotTable[rowIndex + numCols][rowElementIndex] = getElementLabel(rowElement);
 						pivotTable[numCols - 1][rowElementIndex] = rowElement.scope.level.label;
 					});
 					_.each(row, function(column) {
-						if(!column.key.value.coordinates.columnAddresses) {
+						if(!getColumnElements(column)) {
 							if(!!tableTree.Values) {
 								tableTree.Values.push({value: column.value.value, format: column.format});
 							} else {
@@ -53,8 +91,8 @@ angular.module('ThreeSixtyOneView.services')
 							}
 						} else {
 							var branch = [];
-							_.each(column.key.value.coordinates.columnAddresses, function(columnElement, columnIndex) {
-								var columnLabel = columnElement.cellValue.specification.members[0].label;
+							_.each(getColumnElements(column), function(columnElement, columnIndex) {
+								var columnLabel = getElementLabel(columnElement);
 
 								if(columnIndex === 0 && numCols > 1) {
 									tableTree[columnLabel] = branch[columnIndex] = tableTree[columnLabel] || {};
@@ -72,27 +110,7 @@ angular.module('ThreeSixtyOneView.services')
 					});
 				});
 
-				var formPivotTable = function(tree, columnLabels) {
-					if(angular.isArray(tree)) {
-						_.each(tree, function(value, index) {
-							pivotTable[index + numCols][columnIndex] = value.value;
-							helperObject[index + numCols][columnIndex] = value.format;
-						});
-						_.each(columnLabels, function(columnLabel, index) {
-							pivotTable[index] = pivotTable[index] || {};
-							pivotTable[index][columnIndex] = columnLabel;
-							helperObject[index] = helperObject[index] || {};
-						});
-						columnIndex++;
-					} else if(angular.isObject(tree)) {
-						_.each(tree, function(branch, columnLabel) {
-							var newLabels = _.values(columnLabels);
-							newLabels.push(columnLabel);
-							formPivotTable(branch, newLabels);
-						});
-					}
-				};
-				formPivotTable(tableTree, []);
+				formPivotTable(tableTree, numCols, []);
 
 				return {formatted: pivotTable, original: response, helperObject: helperObject};
 			}
