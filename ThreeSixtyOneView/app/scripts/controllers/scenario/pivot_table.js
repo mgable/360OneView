670ed74df048574741bd3ef43f9800081af743d1
Behrooz <bkamali@marketshare.com>
2015-02-26 15:39:32 -0800

    jiraId:MAR-8747 fix issues with column width and word wrap in the pivot table

--- ThreeSixtyOneView/app/scripts/controllers/scenario/pivot_table.js
+++ ThreeSixtyOneView/app/scripts/controllers/scenario/pivot_table.js
@@ -51,7 +51,10 @@ angular.module("ThreeSixtyOneView").controller("pivotTableCtrl", ["$scope", "$ro
 				addColumnStyle = function(){
 					for (var j = 0; j < colCnt; j++) {
 						var column = sheet.getColumn(j);
-						column.vAlign($.wijmo.wijspread.VerticalAlign.center).textIndent(1);
+						column.vAlign($.wijmo.wijspread.VerticalAlign.center);
+						column.shrinkToFit(true);
+						// column.textIndent(1);
+						column.width(sheet.defaults.colWidth);
 						if (j < colHeaderCnt) {
 							column.formatter("0").font(pivotTableConfig.font.headerFontStyle).foreColor(pivotTableConfig.color.msBlack);
 							column.wordWrap(true);
