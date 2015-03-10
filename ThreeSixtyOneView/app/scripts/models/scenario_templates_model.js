baf0b8b30e72322b821fb29e259a00a41e21860c
Behrooz <bkamali@marketshare.com>
2015-03-10 11:07:14 -0700

    create scenario template model and service

--- ThreeSixtyOneView/app/scripts/models/scenario_templates_model.js
+++ ThreeSixtyOneView/app/scripts/models/scenario_templates_model.js
@@ -7,11 +7,12 @@
  * # ModelScenarioTemplatesModelCtrl
  * Controller of the ThreeSixtyOneView
  */
-angular.module('ThreeSixtyOneView')
-  .controller('ModelScenarioTemplatesModelCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('ThreeSixtyOneView.services')
+.factory('ScenarioTemplatesModel', ['$location', 'Resource', 'CONFIG', 'ServerService', function ($location, Resource, CONFIG, ServerService) {
+	var resource = new Resource(ServerService.get($location.host())  + CONFIG.application.api.pivotdata);
+
+	return {
+		resource: resource,
+		config: {}
+	};
+}]);
