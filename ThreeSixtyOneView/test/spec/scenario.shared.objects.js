1a123ae94c87b67934f9dc288194c0cdd7307135
Behrooz <bkamali@marketshare.com>
2015-03-11 16:19:06 -0700

    jiraId:MAR-6150 create a file for shared scenario controller test objects

--- ThreeSixtyOneView/test/spec/scenario.shared.objects.js
+++ ThreeSixtyOneView/test/spec/scenario.shared.objects.js
@@ -1,9 +1,6 @@
-'use strict';
+"use strict";
 
-describe('Service: ExportResourceService', function () {
-
-	var ExportResourceService, ExportResourceModel, backend, exportUrl,
-	elementId = 23,
+var elementId = 23,
 	sampleView = {
 		"id": 1,
 		"name": "Default Marketing Plan view",
@@ -181,54 +178,4 @@ describe('Service: ExportResourceService', function () {
 			}
 		}
 		]
-	};
-
-	// load the controller's module
-	beforeEach(module('ThreeSixtyOneView.services'));
-
-	// setup backend
-	beforeEach(inject(function(SERVER, CONFIG, $httpBackend) {
-		exportUrl = SERVER.server + CONFIG.application.api.exportResource;
-		exportUrl = exportUrl.replace(/:elementId/, elementId);
-		backend = $httpBackend;
-
-		backend.when('POST', exportUrl, sampleView).respond({'status': 'EXPORT_REQUEST_ACCEPTED'});
-		backend.when('GET', exportUrl + '/status').respond({'status': 'COMPLETED'});
-		backend.when('GET', exportUrl + '/download').respond(exportUrl + '/download');
-	}));
-
-	// Initialize the controller and a mock scope
-	beforeEach(inject(function (_ExportResourceService_, _ExportResourceModel_, $httpBackend) {
-		ExportResourceService = _ExportResourceService_;
-		ExportResourceModel = _ExportResourceModel_;
-	}));
-
-	afterEach(function() {
-		backend.verifyNoOutstandingExpectation();
-	});
-
-	it('resource service should be extended to the current scope', function () {
-		expect(ExportResourceService.resource).toBeDefined();
-	});
-
-	it('request an export', function() {
-		ExportResourceService.requestExport(elementId, sampleView).then(function(response) {
-			expect(response.status).toBe('EXPORT_REQUEST_ACCEPTED');
-		});
-		backend.flush();
-	});
-
-	it('check the export status', function() {
-		ExportResourceService.checkStatus(elementId).then(function(response) {
-			expect(response.status).toBe('COMPLETED');
-		});
-		backend.flush();
-	});
-
-	it('download the file', function() {
-		ExportResourceService.downloadFile(elementId).then(function(response) {
-			expect(response).toBe(exportUrl + '/download');
-		});
-		backend.flush();
-	});
-});
+	}
