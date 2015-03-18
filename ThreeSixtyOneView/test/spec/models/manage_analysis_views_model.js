73bb891cf2a1327171af43e813c00b72b182918e
Behrooz <bkamali@marketshare.com>
2015-03-18 16:46:27 -0700

    jiraId:MAR-6150 add unit tests for manage analysis views model and service

---
+++ ThreeSixtyOneView/test/spec/models/manage_analysis_views_model.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: ManageAnalysisViewsModel', function () {
+
+	// load the controller's module
+	beforeEach(module('ThreeSixtyOneView.services'));
+
+	var ManageAnalysisViewsModel;
+
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($rootScope, _ManageAnalysisViewsModel_) {
+		ManageAnalysisViewsModel = _ManageAnalysisViewsModel_;
+	}));
+
+	it('resource service should be extended to the current scope', function () {
+		expect(ManageAnalysisViewsModel.resource).toBeDefined();
+	});
+});
