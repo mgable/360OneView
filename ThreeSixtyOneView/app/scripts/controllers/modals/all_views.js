81a3176c23c9a774adb5b5e7a5673214ed963add
Behrooz <bkamali@marketshare.com>
2015-03-23 10:44:58 -0700

    implement shared list lightbox controller and update current ones

--- ThreeSixtyOneView/app/scripts/controllers/modals/all_views.js
+++ ThreeSixtyOneView/app/scripts/controllers/modals/all_views.js
@@ -4,15 +4,14 @@
 angular.module('ThreeSixtyOneView')
 .controller('AllViewsCtrl', ["$scope", "$controller", "$modalInstance", "CONFIG", "data",
 function($scope, $controller, $modalInstance, CONFIG, data) {
-	angular.extend(this, $controller('ModalBaseCtrl', {$scope: $scope, $modalInstance: $modalInstance, CONFIG: CONFIG}));
+	angular.extend(this, $controller('ListLightboxBaseCtrl', {$scope: $scope, $controller: $controller, $modalInstance: $modalInstance, CONFIG: CONFIG}));
 
 	var init = function() {
+		$scope.list = data.viewsList;
 		$scope.e2e = data.e2e;
 
 		$scope.subtitle = 'Select A ' + (data.subTitle || 'View');
 		$scope.isDropdownHidden = true;
-		$scope.cancelButtonLabel = 'Cancel';
-		$scope.submitButtonLabel = 'Replace';
 		$scope.dateProperty = 'createdOn';
 		$scope.ownerProperty = 'createdBy';
 		$scope.isListLoaded = true;
@@ -20,17 +19,6 @@ function($scope, $controller, $modalInstance, CONFIG, data) {
 		$scope.currentItem = {
 			id: data.selectedViewId
 		};
-	},
-	viewsList = data.viewsList;
-
-	$scope.getList = function(){
-		return viewsList;
-	};
-
-	// cancel the changes and dismiss the modal
-	$scope.cancel = function() {
-		viewsList = [];
-		$modalInstance.dismiss('canceled');
 	};
 
 	// pass back the selected file and dismiss the modal
---
+++ ThreeSixtyOneView/app/scripts/controllers/modals/list_lightbox_base.js
@@ -0,0 +1,25 @@
+/* global _ */
+'use strict';
+
+angular.module('ThreeSixtyOneView')
+.controller('ListLightboxBaseCtrl', ["$scope", "$controller", "$modalInstance", "CONFIG",
+function($scope, $controller, $modalInstance, CONFIG, data, MetaDataService) {        
+	angular.extend(this, $controller('ModalBaseCtrl', {$scope: $scope, $modalInstance: $modalInstance, CONFIG: CONFIG}));
+
+	$scope.list = [];
+	$scope.isListLoaded = false;
+	$scope.cancelButtonLabel = 'Cancel';
+	$scope.submitButtonLabel = 'Replace';
+	$scope.dateProperty = 'lastUpdatedOn';
+	$scope.ownerProperty = 'lastUpdatedBy';
+
+	$scope.getList = function() {
+		return $scope.list;
+	};
+
+	// cancel the changes and dismiss the modal
+	$scope.cancel = function() {
+		$scope.fileList = [];
+		$modalInstance.dismiss('canceled');
+	};
+}]);
