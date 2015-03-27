962474f9c57c70c1c880f5a86dc750ead73dde32
Behrooz <bkamali@marketshare.com>
2015-03-27 16:44:58 -0700

    jiraId:MAR-9006 add default row and column to the scenario template defaults page and controller

--- ThreeSixtyOneView/app/scripts/controllers/scenario_templates/scenario_templates_defaults.js
+++ ThreeSixtyOneView/app/scripts/controllers/scenario_templates/scenario_templates_defaults.js
@@ -8,21 +8,79 @@
  * Controller of the ThreeSixtyOneView
  */
 angular.module('ThreeSixtyOneView')
-.controller('ScenarioTemplatesDefaultsCtrl', ['$scope', '$filter', 'datepickerConfig', function ($scope, $filter, datepickerConfig) {
+.controller('ScenarioTemplatesDefaultsCtrl', ['$scope', 'PivotMetaService', 'PivotViewService', 'datepickerConfig', 'MetaDataService',
+	function ($scope, PivotMetaService, PivotViewService, datepickerConfig, MetaDataService) {
 
 	var init = function() {
-		$scope.dateFormat = 'yyyy-MM-dd Week (ww)';
-		$scope.fromDate = new Date();
-		$scope.toDate = new Date();
+			$scope.pivotBuilderItems = [{name:'columns', label: 'Columns', other: 'rows'}, {name:'rows', label: 'Rows', other: 'columns'}];
+			$scope.dimensions = [];
+			$scope.viewData = {};
+			MetaDataService.buildDimensionsTree(1).then(function(dimensions) {
+				$scope.dimensions = dimensions;
+				$scope.viewData = PivotMetaService.formEmptyView(dimensions, {label: ''});
+				$scope.added = PivotMetaService.setUpAddedLevels($scope.viewData.rows.concat($scope.viewData.columns));
+				determineTimeDisability($scope.added);
+			});
 
-		// datepicker options
-		datepickerConfig.minDate = '2014-01-02';
-		datepickerConfig.maxDate = '2016-01-01';
-		datepickerConfig.startingDay = 1;
+			$scope.dragOptions = {
+				itemMoved: function() {
+					// $scope.saveDraftView();
+				},
+				orderChanged: function() {
+					// $scope.saveDraftView();
+				},
+				dragStart: function() {
+					$scope.isDragging = true;
+				},
+				dragEnd: function() {
+					$scope.isDragging = false;
+				},
+				containment: '#dragDropArea'
+			};
+
+			initDatePicker();
+		},
+		initDatePicker = function() {
+			$scope.dateFormat = 'yyyy-MM-dd Week (ww)';
+			$scope.fromDate = new Date();
+			$scope.toDate = new Date();
+
+			// datepicker options
+			datepickerConfig.minDate = '2014-01-02';
+			datepickerConfig.maxDate = '2016-01-01';
+			datepickerConfig.startingDay = 1;
+		},
+		determineTimeDisability = function(added) {
+			$scope.timeDisabled = PivotMetaService.determineTimeDisability($scope.dimensions, added, $scope.membersList);
+		};
+
+	// returns element titles in the view: rows and columns
+	$scope.getPivotBuilderItems = function() {
+		return $scope.pivotBuilderItems;
 	};
 
 	$scope.getFormattedDate = function(date) {
 		return $filter('date')(date.getTime(),'yyyy-MM-dd');
+	};
+
+	$scope.getViewData = function(element) {
+		return $scope.viewData[element];
+	};
+
+	$scope.getDimensions = function() {
+		return $scope.dimensions;
+	};
+
+	$scope.deleteItem =  function(index, element) {
+		PivotViewService.deleteItem($scope.viewData, $scope.added, index, element, [determineTimeDisability]);
+	};
+
+	$scope.addItem = function(item, element) {
+		PivotViewService.addItem($scope.viewData, $scope.added, item, element, [determineTimeDisability]);
+	};
+
+	$scope.replaceItem = function(selected, priorLabel, element) {
+		PivotViewService.replaceItem($scope.viewData, $scope.added, selected, priorLabel, element, [determineTimeDisability]);
 	};
 
 	init();
