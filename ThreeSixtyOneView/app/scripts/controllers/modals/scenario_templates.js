b95ce7e8efb6702c7ef55c7372e430fba0b629b9
Behrooz <bkamali@marketshare.com>
2015-04-02 14:39:49 -0700

    jiraId:MAR-9005 change the whole scenario templates workflow from a page to a lightbox and add first welcome page

--- ThreeSixtyOneView/app/scripts/controllers/modals/scenario_templates.js
+++ ThreeSixtyOneView/app/scripts/controllers/modals/scenario_templates.js
@@ -1,17 +1,19 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name ThreeSixtyOneView.controller:ModalScenarioTemplatesCtrl
- * @description
- * # ModalScenarioTemplatesCtrl
- * Controller of the ThreeSixtyOneView
- */
 angular.module('ThreeSixtyOneView')
-  .controller('ModalScenarioTemplatesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('ScenarioTemplatesCreateCtrl', ["$scope", "$controller", "$modalInstance", "CONFIG", "data",
+function($scope, $controller, $modalInstance, CONFIG, data) {
+	angular.extend(this, $controller('ModalBaseCtrl', {$scope: $scope, $controller: $controller, $modalInstance: $modalInstance, CONFIG: CONFIG}));
+
+	var init = function() {
+		$scope.CONFIG = CONFIG;
+		$scope.currentType = data.templateType;
+	};
+
+	// pass back the selected file and dismiss the modal
+	$scope.submit = function() {
+		$modalInstance.close({response:'response'});
+	};
+
+	init();
+}]);
