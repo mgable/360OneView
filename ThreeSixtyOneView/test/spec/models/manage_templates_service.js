7a63e8d60bb484e6fa5400e1db559d17b68c03f1
Behrooz <bkamali@marketshare.com>
2015-04-06 15:43:45 -0700

    jiraId:MAR-9005 add manage scenario templates unit tests

--- ThreeSixtyOneView/test/spec/models/manage_templates_service.js
+++ ThreeSixtyOneView/test/spec/models/manage_templates_service.js
@@ -2,17 +2,48 @@
 
 describe('Service: ManageTemplatesService', function () {
 
-// load the controller's module
+	var ManageTemplatesService, ManageTemplatesModel, backend, manageTemplatesUrl;
+
+	// load the controller's module
 	beforeEach(module('ThreeSixtyOneView.services'));
 
-	var ManageTemplatesService;
+	beforeEach(inject(function(SERVER, CONFIG, $httpBackend) {
+		manageTemplatesUrl = SERVER.server + CONFIG.application.api.template;
+		backend = $httpBackend;
+
+		backend.when('GET', manageTemplatesUrl.replace(/\/:templateId/,'')).respond(JSON.parse(scenarioTemplates));
+		backend.when('GET', manageTemplatesUrl.replace(/:templateId/, templateId)).respond(JSON.parse(scenarioTemplates)[0]);
+	}));
 
 	// Initialize the controller and a mock scope
 	beforeEach(inject(function ($rootScope, _ManageTemplatesService_) {
 		ManageTemplatesService = _ManageTemplatesService_;
 	}));
 
+	afterEach(function() {
+		backend.verifyNoOutstandingExpectation();
+	});
+
 	it('resource service should be extended to the current scope', function () {
 		expect(ManageTemplatesService.resource).toBeDefined();
+	});
+
+	describe('API calls', function() {
+		afterEach(function() {
+			backend.flush();
+		});
+
+		it('get list of all scenario templates', function() {
+			ManageTemplatesService.getAll().then(function(response) {
+				expect(response).toEqual(JSON.parse(scenarioTemplates));
+			});
+		});
+
+		it('get a specific scenario template', function() {
+			ManageTemplatesService.get(templateId).then(function(response) {
+				var templates = JSON.parse(scenarioTemplates);
+				expect(response).toEqual(templates[0]);
+			});
+		});
 	});
 });
