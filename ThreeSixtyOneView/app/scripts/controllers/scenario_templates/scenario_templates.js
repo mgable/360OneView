b180fcd10ca9db73ec855f07ee4d5710a108c07c
Behrooz <bkamali@marketshare.com>
2015-04-07 11:53:37 -0700

    jiraId:MAR-9131 add redirect to master project on scenario template create cancel

--- ThreeSixtyOneView/app/scripts/controllers/scenario_templates/scenario_templates.js
+++ ThreeSixtyOneView/app/scripts/controllers/scenario_templates/scenario_templates.js
@@ -1,31 +1,50 @@
 'use strict';
 
 angular.module('ThreeSixtyOneView')
-.controller('ScenarioTemplatesCtrl',['$scope', '$state', 'DialogService', 'CONFIG',
-	function ($scope, $state, DialogService, CONFIG) {
-		var init = function() {
-			$scope.currentType = _.find(CONFIG.view.ScenarioTemplates.types, function(type) {
-				return type.name === $state.params.type;
-			});
+.controller('ScenarioTemplatesCtrl',['$scope', '$state', 'DialogService', 'CONFIG', 'ManageTemplatesService', 'ProjectsService',
+	function ($scope, $state, DialogService, CONFIG, ManageTemplatesService, ProjectsService) {
+		var masterProject,
+			init = function() {
+				$scope.onboardingState = false;
+				ManageTemplatesService.getAll().then(function(response) {
+					$scope.scenarioTemplates = response;
+					if(response.length === 0) {
+						$scope.onboardingState = true;
+					} else {
+						$scope.openModulePickDialog();
+					}
+				});
+				ProjectsService.get().then(function(response) {
+					masterProject = _.find(response, function(elem){return elem.isMaster;});
+				});
+				$scope.currentType = _.find(CONFIG.view.ScenarioTemplates.types, function(type) {
+					return type.name === $state.params.type;
+				});
 
-			if(typeof $scope.currentType !== 'undefined') {
-				openScenarioTemplatesCreateModal($scope.currentType);
-			}
-		}, openScenarioTemplatesCreateModal = function(type, templates) {
-			var scenarioTemplates = templates,
-				dialog = DialogService.openLightbox('views/modal/scenario_templates.tpl.html', 'ScenarioTemplatesCreateCtrl',
-					{templateType: type, scenarioTemplates: $scope.scenarioTemplates},
-					{windowSize: 'lg', windowClass: 'scenario_templates'}
-				);
+				if(typeof $scope.currentType !== 'undefined') {
+					openScenarioTemplatesCreateModal($scope.currentType);
+				}
+			}, openScenarioTemplatesCreateModal = function(type, templates) {
+				var scenarioTemplates = templates,
+					dialog = DialogService.openLightbox('views/modal/scenario_templates.tpl.html', 'ScenarioTemplatesCreateCtrl',
+						{templateType: type, scenarioTemplates: $scope.scenarioTemplates},
+						{windowSize: 'lg', windowClass: 'scenario_templates'}
+					);
 
-			dialog.result.then(function(data) {
-				createTemplate(data);
-			});
-		}, createTemplate = function(data) {
-			console.log('create template');
-			console.log('create default view');
-			console.log(data);
-		};
+				dialog.result.then(function(data) {
+					createTemplate(data);
+				}, function() {
+					if(!$scope.onboardingState) {
+						gotoProject(masterProject.uuid);
+					}
+				});
+			}, createTemplate = function(data) {
+				console.log('create template');
+				console.log('create default view');
+				console.log(data);
+			}, gotoProject = function(uuid) {
+				$state.go('Dashboard', {projectId: uuid});
+			};
 
 		$scope.openModulePickDialog = function() {
 			var modulePickDialog = DialogService.openLightbox('views/modal/module_pick.tpl.html', 'ModulePickCtrl',
@@ -34,6 +53,10 @@ angular.module('ThreeSixtyOneView')
 
 			modulePickDialog.result.then(function(data) {
 				openScenarioTemplatesCreateModal(data.selectedModule);
+			}, function() {
+				if(!$scope.onboardingState) {
+					gotoProject(masterProject.uuid);
+				}
 			});
 		};
 
