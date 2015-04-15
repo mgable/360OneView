955c3dc04e309d89586bdaea3a5b9148f6585ee0
Mark Gable <mgable@marketshare.com>
2015-04-15 16:52:46 -0700

    Create Recommendation

---
+++ ThreeSixtyOneView/app/scripts/controllers/create_recommendation/create_recommendation.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name threeSixtOneViewApp.controller:CreaterecommendationCtrl
+ * @description
+ * # CreaterecommendationCtrl
+ * Controller of the threeSixtOneViewApp
+ */
+angular.module('ThreeSixtyOneView')
+  .controller('CreateRecommendationCtrl', function ($scope) {
+    $scope.test = "I am working"
+  });
---
+++ ThreeSixtyOneView/app/scripts/controllers/create_recommendation/select.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name threeSixtOneViewApp.controller:CreaterecommendationCtrl
+ * @description
+ * # CreaterecommendationCtrl
+ * Controller of the threeSixtOneViewApp
+ */
+angular.module('ThreeSixtyOneView')
+  .controller('CreateRecommendationSelectCtrl', function ($scope) {
+    $scope.test = "I am working";
+  });
