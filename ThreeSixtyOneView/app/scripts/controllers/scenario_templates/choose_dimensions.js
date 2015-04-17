6a695cd55bf0254b8688ccb238bcfaf684f77ced
Chengchang Qian <cqian@marketshare.com>
2015-04-16 17:07:37 -0700

    jiraID: MAR-9007, update selection to parent controller

--- ThreeSixtyOneView/app/scripts/controllers/scenario_templates/choose_dimensions.js
+++ ThreeSixtyOneView/app/scripts/controllers/scenario_templates/choose_dimensions.js
@@ -11,47 +11,46 @@ angular.module('ThreeSixtyOneView')
     .controller('ChooseDimensionsCtrl', ['$scope', 'MetaDataService', 'DimensionService', 'EVENTS', function($scope, MetaDataService, DimensionService, EVENTS) {
         var scenarioTemplateId,
         init = function() {
+            $scope.selectedTime = $scope.timeGranularity ? $scope.timeGranularity : '';
             buildDimensions($scope.dimensionsList, $scope.kpisList);
+            if (checkIfInitial($scope.dimensionsList) && checkIfInitial($scope.kpisList)) {
+                addSelectedValue($scope.kpiDimensions);
+                addSelectedValue($scope.standardDimensions);
+            }
+        },
+        checkIfInitial = function(dimensionsList) {
+            var initial = true;
+            _.each(dimensionsList, function(dimension) {
+                if (dimension.isSelected !== undefined) {
+                    initial = false;
+                }
+            });
+            return initial;
+        },
+        addSelectedValue = function(dimensions) {
+            _.each(dimensions, function(dimension) {
+                dimension.isSelected = dimension.isSelected || true;
+                var childrenList = dimension.members ? dimension.members : dimension.attributes;
+                _.each(childrenList, function(children) {
+                    children.isSelected = children.isSelected || true;
+                });
+            });
+            return dimensions;
         },
         buildDimensions = function(dimensionsList, kpisList) {
-
-            var addSelectedValue = function(dimensions) {
-                _.each(dimensions, function(dimension) {
-                    dimension.isSelected = dimension.isSelected || true;
-                    var childrenList = dimension.members ? dimension.members : dimension.attributes;
-                    _.each(childrenList, function(children) {
-                        children.isSelected = children.isSelected || true;
-                    });
-                });
-                return dimensions;
-            };
-
             // filter to get time dimensions
             $scope.timeDimension = _.find(dimensionsList, function(dimension) { return dimension.type === 'TimeDimension' });
             $scope.times = _.pluck($scope.timeDimension.attributes, 'label');
-
-            // filter to get kpi dimension
+            // filter to get kpi dimensions
             $scope.kpiDimensions = kpisList;
-            addSelectedValue($scope.kpiDimensions);
-
+            // filter to get standard dimensions
             $scope.standardDimensions = _.filter(dimensionsList, function(dimension) { return dimension.type === 'StandardDimension' });
-            addSelectedValue($scope.standardDimensions);
-
         };
 
 		$scope.$on(EVENTS.flipbookAdvance, function() {
-
             $scope.setTimeGranularity($scope.selectedTime);
             $scope.setDimensionsLabel($scope.standardDimensions, 'standard');
             $scope.setDimensionsLabel($scope.kpiDimensions, 'kpi');
-
-            var cubes = {
-                time: DimensionService.getSelectedTimeDimension($scope.timeDimension, $scope.selectedTime) || {},
-                standard: DimensionService.getSelectedDimensions($scope.standardDimensions) || [],
-                kpi: DimensionService.getSelectedDimensions($scope.kpiDimensions) || []
-            };
-            console.info('cubes: ', cubes);
-
 		});
 
         $scope.$watch("selectedTime", function(){
