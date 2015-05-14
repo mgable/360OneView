92ee9193a61001666bd0b216385c67be48fa19f2
Mark Gable <mgable43@gmail.com>
2015-05-14 09:13:24 -0700

    utility program

---
+++ server/rename.js
@@ -0,0 +1,22 @@
+"use strict";
+var fs = require("fs");
+var args = process.argv.slice(2);
+
+readDirectory(args[0]);
+
+function readDirectory(directory){
+	var contents = fs.readdirSync(directory);
+	contents.forEach(function(i){
+		var directoryAndFileName = directory + "/" + i,
+			stats = (fs.statSync(directoryAndFileName));
+		if(stats.isFile()){
+			if(!/\.json$/.test(i)){
+				console.info("renaming " + i );
+				fs.renameSync(directoryAndFileName, directoryAndFileName + ".json");
+			}
+		} else {
+			//console.info(i + " is a directory");
+			readDirectory(directoryAndFileName);
+		}
+	});
+}
