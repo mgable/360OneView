8e9ee6db260e7144ac179a4c255e991f7d21c482
Behrooz <bkamali@marketshare.com>
2015-05-21 17:42:14 -0700

    jiraId:MAR-9005 complete pivot meta service unit tests

--- ThreeSixtyOneView/test/spec/services/pivot_meta_service.js
+++ ThreeSixtyOneView/test/spec/services/pivot_meta_service.js
@@ -8,6 +8,7 @@ describe('Service: PivotMetaService', function () {
 
 	var viewData = JSON.parse(scenarioMockData.touchpointView),
 		dimensions = JSON.parse(scenarioMockData.dimensionTree),
+		cubeMeta = JSON.parse(scenarioMockData.cubeMeta),
 		addedFilters,
 		membersList;
 
@@ -15,6 +16,29 @@ describe('Service: PivotMetaService', function () {
 		PivotMetaService = _PivotMetaService_;
 		MetaDataService = _MetaDataService_;
 		ManageAnalysisViewsService = _ManageAnalysisViewsService_;
+
+		var dimensionsPromise = function() {
+			return {
+				then: function(callback) {
+					callback(dimensions);
+					return this;
+				}
+			};
+		};
+
+		var viewsPromise = function() {
+			return {
+				then: function(callback) {
+					callback([viewData]);
+					return this;
+				}
+			};
+		};
+
+		spyOn(ManageAnalysisViewsService, 'createView').and.callThrough();
+		spyOn(ManageAnalysisViewsService, 'updateView').and.callThrough();
+		spyOn(MetaDataService, 'buildDimensionsTree').and.callFake(dimensionsPromise);
+		spyOn(ManageAnalysisViewsService, 'getViewsList').and.callFake(viewsPromise);
 	}));
 
 	it('should be defined', function () {
@@ -59,11 +83,104 @@ describe('Service: PivotMetaService', function () {
 		}
 	});
 
-	it('should generate flas list of all the members for a given dimension', function() {
+	it('should generate flat list of all the members for a given dimension', function() {
 		membersList = PivotMetaService.generateMembersList(dimensions);
 
 		for(var i = 0; i < dimensions.length; i++) {
 			expect(membersList[dimensions[i].id]).toBeDefined();
 		}
+	});
+
+	it('should generate the categorized and count values for the whole pivot view', function() {
+		var i, categorizedValues = PivotMetaService.generateCategorizeValueStructure(addedFilters, dimensions, viewData);
+
+		for(i = 0; i < dimensions.length; i++) {
+			expect(categorizedValues[i]).toBeDefined();
+			expect(categorizedValues[i].label.length > 0).toBeTruthy();
+			expect(categorizedValues[i].id.length > 0).toBeTruthy();
+			expect(categorizedValues[i].total >= categorizedValues[i].selected).toBeTruthy();
+		}
+	});
+
+	it('should form an empty view with all the default values', function() {
+		var newView = PivotMetaService.formEmptyView(dimensions, cubeMeta);
+
+		expect(newView.name).toBe('Default ' + cubeMeta.label + ' view');
+		expect(newView.isDraft).toBeFalsy();
+		expect(newView.isDefault).toBeTruthy();
+		expect(newView.rows.length).toBe(1);
+		expect(newView.columns.length).toBe(1);
+		expect(newView.filters.length).toBe(dimensions.length);
+		for(var i = 0; i < dimensions.length; i++) {
+			if(dimensions[i].type === 'TimeDimension') {
+				expect(newView.filters[i].value.specification.type).toBe('Absolute');
+			} else {
+				expect(newView.filters[i].value.specification.type).toBe('All');
+			}
+		}
+	});
+
+	it('should create an empty view', function() {
+		PivotMetaService.createEmptyView(dimensions, cubeMeta);
+
+		expect(ManageAnalysisViewsService.createView).toHaveBeenCalled();
+	});
+
+	it('should update the view', function() {
+		PivotMetaService.updateView(cubeMeta.id, viewData);
+		var i, modifiedView = angular.copy(viewData);
+		for(i = 0; i < dimensions.length; i++) {
+			modifiedView.filters[i].id = 0;
+		}
+
+		expect(ManageAnalysisViewsService.updateView).toHaveBeenCalledWith(modifiedView, cubeMeta.id);
+	});
+
+	it('should find the default view in the views list', function() {
+		var i, tempView, list = [];
+		for(i = 0; i < 5; i++) {
+			tempView = angular.copy(viewData);
+			tempView.id = i;
+			tempView.isDraft = false;
+			list.push(tempView);
+		}
+		list[2].isDraft = true;
+
+		expect(PivotMetaService.findDefaultView(list)).toBe(2);
+	});
+
+	it('should initialize the model by getting dimensions and views', function() {
+		PivotMetaService.initModel(cubeMeta);
+
+		expect(MetaDataService.buildDimensionsTree).toHaveBeenCalledWith(cubeMeta.id);
+		expect(ManageAnalysisViewsService.getViewsList).toHaveBeenCalledWith(cubeMeta.id);
+	});
+
+	it('should generate the object containing added rows and columns in the pivot view', function() {
+		var items = [].concat(viewData.rows, viewData.columns),
+			added = PivotMetaService.setUpAddedLevels(items),
+			i;
+
+		for(i = 0; i < items.length; i++) {
+			expect(added[items[i].level.label]).toBeTruthy();
+		}
+	});
+
+	it('should generate the filters object in the server format from the ui format', function() {
+		var filters = PivotMetaService.updateFilters(dimensions, addedFilters, membersList, viewData.filters);
+
+		expect(filters.length).toBe(dimensions.length);
+		for(var i = 0; i < filters.length; i++) {
+			expect(filters[i].id).toBe(0);
+			expect(filters[i].value.specification.type).toBeDefined();
+			expect(filters[i].scope).toBeDefined();
+		}
+	});
+
+	it('should determine whether time dimension members can be added to the pivot view', function() {
+		var disability = PivotMetaService.determineTimeDisability(dimensions, PivotMetaService.setUpAddedLevels([].concat(viewData.rows, viewData.columns)));
+		expect(disability).toBeTruthy();
+		disability = PivotMetaService.determineTimeDisability(dimensions, PivotMetaService.setUpAddedLevels([]));
+		expect(disability).toBeFalsy();
 	});
 });
