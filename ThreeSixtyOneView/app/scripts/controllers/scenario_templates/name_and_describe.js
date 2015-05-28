d8ccc8370242ff7ffb0a0138cfc42d671d4eaf9a
Behrooz <bkamali@marketshare.com>
2015-05-28 10:27:45 -0700

    jiraId:MAR-11041 disable next button when scenario template name is not unique and display related error message

--- ThreeSixtyOneView/app/scripts/controllers/scenario_templates/name_and_describe.js
+++ ThreeSixtyOneView/app/scripts/controllers/scenario_templates/name_and_describe.js
@@ -8,23 +8,27 @@
  * Controller of the ThreeSixtyOneView
  */
 angular.module('ThreeSixtyOneView')
-    .controller('NameAndDescribeCtrl', ["$scope", "EVENTS", function($scope, EVENTS) {
+.controller('NameAndDescribeCtrl', ["$scope", "EVENTS", function($scope, EVENTS) {
 
-        $scope.$watch("template.name", function(){
-            if ($scope.template.name && $scope.template.name.length > 0 && $scope.dimensionsIsLoaded){
-                $scope.$emit(EVENTS.flipbookAllowAdvance, true);
-            } else {
-                $scope.$emit(EVENTS.flipbookAllowAdvance, false);
-            }
-        });
+	$scope.isTemplateNameUnique = function(templateName) {
+		return !_.findWhere($scope.scenarioTemplates, {name: templateName});
+	};
 
-    	$scope.$on(EVENTS.flipbookAdvance, function() {
-			$scope.createDraftTemplate();
-		});
+	$scope.$watch("template.name", function(){
+		if ($scope.template.name && $scope.template.name.length > 0 && $scope.dimensionsIsLoaded && $scope.isTemplateNameUnique($scope.template.name)){
+			$scope.$emit(EVENTS.flipbookAllowAdvance, true);
+		} else {
+			$scope.$emit(EVENTS.flipbookAllowAdvance, false);
+		}
+	});
 
-        $scope.$on(EVENTS.dimensionsIsLoaded, function() {
-             if ($scope.template.name && $scope.template.name.length > 0){
-                $scope.$emit(EVENTS.flipbookAllowAdvance, true);
-            } 
-        });
-    }]);
+	$scope.$on(EVENTS.flipbookAdvance, function() {
+		$scope.createDraftTemplate();
+	});
+
+	$scope.$on(EVENTS.dimensionsIsLoaded, function() {
+		 if ($scope.template.name && $scope.template.name.length > 0){
+			$scope.$emit(EVENTS.flipbookAllowAdvance, true);
+		} 
+	});
+}]);
