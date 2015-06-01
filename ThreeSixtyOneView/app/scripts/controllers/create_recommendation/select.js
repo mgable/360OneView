2e0070b207211ecfd97a5a7f6f05199f427f3f3b
Behrooz <bkamali@marketshare.com>
2015-06-01 15:34:26 -0700

    jiraId:MAR-10961 fix visual design bugs in create recommendation page

--- ThreeSixtyOneView/app/scripts/controllers/create_recommendation/select.js
+++ ThreeSixtyOneView/app/scripts/controllers/create_recommendation/select.js
@@ -33,7 +33,7 @@ function ($scope, $q, EVENTS, PivotMetaService, DialogService, ManageScenariosSe
 				$scope.membersList = PivotMetaService.generateMembersList(spendDimensions);
 				$scope.categorizedValue = PivotMetaService.generateCategorizeValueStructure($scope.addedFilters, spendDimensions, $scope.viewData);
 				return view;
-			},function() {alert(arguments);});
+			});
 		},
 		getAnalysisElement = function getAnalysisElement(baseScenario, spendCubeId) {
 			return ManageScenariosService.getAnalysisElementByScenarioAndCube(baseScenario.id, spendCubeId).then(function(analysisElement) {
